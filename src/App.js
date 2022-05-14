bb81a323e89180fc531368ef60f9506f2a3cf8ac
Yakutmuder9 <yakutmuder9@gmail.com>
2022-05-14 12:46:12 -0400

    new Callenge

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
-    return (
-        <div className='App'>
-            <Navbar dark color='primary' sticky='top' expand='md'>
-                <Container>
-                    <NavbarBrand href='/'>
-                        <img src={NucampLogo} alt='nucamp logo' />
-                    </NavbarBrand>
-                </Container>
-            </Navbar>
-            I'm ready for workshop!
-        </div>
-    );
+	return (
+		<div className="App">
+			<Header />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="contact" element={<ContactPage />} />
+				<Route path="directory" element={<CampsitesDirectoryPage />} />
+				<Route
+					path="directory/:campsiteId"
+					element={<CampsiteDetailPage />}
+				/>
+				<Route path="about" element={<AboutPage />} />
+			</Routes>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
